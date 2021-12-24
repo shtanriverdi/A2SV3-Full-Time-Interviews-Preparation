3407b7986cfdd3b1196a638545aa9e6bf4d76c36
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2021-12-24 21:47:36 +0300

    Add files via upload

---
+++ Daily Questions/50A - Domino piling.cpp
@@ -0,0 +1,13 @@
+// Question Link: https://codeforces.com/problemset/problem/50/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int M, N;
+	cin >> M >> N;
+
+	cout << (M * N) / 2;
+
+	return 0;
+}
