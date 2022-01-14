12c805c5b44e41c83931fa52fe0bf45a51a1892e
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2022-01-14 19:03:27 +0300

    Add files via upload

---
+++ Daily Questions/876. Middle of the Linked List.cpp
@@ -0,0 +1,16 @@
+// Question Link: https://leetcode.com/problems/middle-of-the-linked-list/
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while (fast && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        return slow;
+    }
+};
