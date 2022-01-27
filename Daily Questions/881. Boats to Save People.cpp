cb37a0e2c625caf0419d19dab939d6484a50f23e
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2022-01-27 18:16:19 +0300

    Add files via upload

---
+++ Daily Questions/881. Boats to Save People.cpp
@@ -0,0 +1,21 @@
+// Question Link: https://leetcode.com/problems/boats-to-save-people/
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int l = 0, len = people.size(), r = len - 1, result = 0;
+        while (l < r) {
+            if (people[r] + people[l] <= limit) {
+                result++;
+                r--;
+                l++;
+            }
+            else {
+                result++;
+                r--;
+            }
+        }
+        return result + (l == r);
+    }
+};
