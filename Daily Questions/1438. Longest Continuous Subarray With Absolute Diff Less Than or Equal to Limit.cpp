bc006e62ca563cfb9346ceec04f6ad9cee615196
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2022-02-02 11:08:41 +0300

    Update 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp

--- Daily Questions/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
+++ Daily Questions/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
@@ -1,5 +1,47 @@
 // Question Link: https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/
 
+// Monotonic Queue Solution O(N)
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        int L = 0, R = 0, result = 0, len = nums.size();
+        deque<int> maxDeque, minDeque;
+        for (; R < len; R++) {
+            updateQueues(minDeque, maxDeque, nums[R]);
+            int minn = minDeque.front(); // Get min
+            int maxx = maxDeque.front(); // Get max
+            int diff = maxx - minn;
+            // Update L pointer and Window
+            if (diff > limit) {
+                if (nums[L] == minDeque.front()) {
+                    minDeque.pop_front();
+                }
+                if (nums[L] == maxDeque.front()) {
+                    maxDeque.pop_front();
+                }
+                L++;
+            }
+            int windowLen = R - L + 1;
+            result = max(windowLen, result);
+        }
+        return result;
+    }
+    
+    void updateQueues(deque<int> &minDeque, deque<int> &maxDeque, int val) {
+        // Update min deque
+        while (!minDeque.empty() && val < minDeque.back()) {
+            minDeque.pop_back();
+        }
+        minDeque.push_back(val);
+        // Update max deque
+        while (!maxDeque.empty() && val > maxDeque.back()) {
+            maxDeque.pop_back();
+        }
+        maxDeque.push_back(val);
+    }
+};
+
+// Map Solution O(NlogN)
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
