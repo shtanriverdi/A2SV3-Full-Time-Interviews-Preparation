6e73bb90dc365f83202fa1c3c70b8523cf295de4
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2022-02-03 19:16:14 +0300

    Update 1031. Maximum Sum of Two Non-Overlapping Subarrays.cpp

--- Daily Questions/1031. Maximum Sum of Two Non-Overlapping Subarrays.cpp
+++ Daily Questions/1031. Maximum Sum of Two Non-Overlapping Subarrays.cpp
@@ -1,5 +1,30 @@
 // Question Link: https://leetcode.com/problems/maximum-sum-of-two-non-overlapping-subarrays/
 
+// O(N)
+class Solution {
+public:
+    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
+        int len = nums.size();
+        for (int i = 1; i < len; i++) {
+            nums[i] += nums[i - 1];
+        }
+        int A = getMaxSum(nums, len, firstLen, secondLen);
+        int B = getMaxSum(nums, len, secondLen, firstLen);
+        return max(A, B);
+    }
+    
+    int getMaxSum(vector<int> &nums, int &len, int f, int s) {
+        int maxFirstWindowSum = 0, maxSum = 0;
+        for (int i = 0; (i + f + s - 1) < len; i++) {
+            maxFirstWindowSum = max(maxFirstWindowSum, nums[i + f - 1] - (i - 1 >= 0 ? nums[i - 1] : 0));
+            int secondWindowCurSum = nums[i + f + s - 1] - ((i + f - 1) >= 0 ? nums[i + f - 1] : 0);
+            maxSum = max(maxSum, maxFirstWindowSum + secondWindowCurSum);
+        }
+        return maxSum;
+    }
+};
+
+// O(N^2)
 class Solution {
 public:
     int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
