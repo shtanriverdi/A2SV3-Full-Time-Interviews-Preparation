fe83c4c4d12921a1292868e0d20698ef79311ea6
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2022-02-09 15:26:56 +0300

    Update 274. H-Index.cpp

--- Daily Questions/274. H-Index.cpp
+++ Daily Questions/274. H-Index.cpp
@@ -1,5 +1,34 @@
 // Question Link: https://leetcode.com/problems/h-index/
 
+// O(N + K) Solution
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        vector<int> counts(1001, 0);
+        for (int &citation : citations) {
+            counts[citation]++;
+        }        
+        
+        vector<int> sorted;
+        for (int i = 1000; i >= 0; i--) {
+            while (counts[i] > 0) {
+                sorted.push_back(i);
+                counts[i]--;
+            }
+        }
+
+        int hIndex = 0;
+        for (; hIndex < sorted.size(); hIndex++) {
+            if (sorted[hIndex] < hIndex + 1) {
+                break;
+            }
+        }
+        
+        return hIndex;
+    }
+};
+
+// O(NlogN) Solution
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
