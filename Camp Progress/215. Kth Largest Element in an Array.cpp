b484e23d152e09f6def7429fe71dd8808ce7f938
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2022-02-24 16:21:32 +0300

    Update 215. Kth Largest Element in an Array.cpp

--- Camp Progress/215. Kth Largest Element in an Array.cpp
+++ Camp Progress/215. Kth Largest Element in an Array.cpp
@@ -1,5 +1,26 @@
 // Question Link: https://leetcode.com/problems/kth-largest-element-in-an-array/
 
+// O(N + K)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int counts[20001] = { 0 };
+        for (int &num : nums) {
+            counts[num + 10000]++;
+        }
+        
+        vector<int> sorted;
+        for (int i = 20000; i >= 0 ; i--) {
+            while (counts[i]-- > 0) {
+                sorted.push_back(i - 10000);
+            }
+        }
+        
+        return sorted[k - 1];
+    }
+};
+
+// O(NlogN)
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
