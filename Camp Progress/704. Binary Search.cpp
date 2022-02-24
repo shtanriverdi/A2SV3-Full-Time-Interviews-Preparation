77e68d4f261fae0b225847217dae8202641944b9
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2022-02-24 14:49:49 +0300

    Update 704. Binary Search.cpp

--- Camp Progress/704. Binary Search.cpp
+++ Camp Progress/704. Binary Search.cpp
@@ -1,5 +1,33 @@
 // Question Link: https://leetcode.com/problems/binary-search/
 
+// Recursive Solution
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int right = nums.size() - 1;
+        return binarySearch(nums, target, 0, right);
+    }
+    
+    int binarySearch(vector<int>& nums, int &target, int left, int right) {
+        if (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (target > nums[mid]) {
+                return binarySearch(nums, target, mid + 1, right);
+            }
+            else {
+                return binarySearch(nums, target, left, mid - 1);
+            }
+        }
+        else {
+            return -1;
+        }
+    }
+};
+
+// Iterative Solution
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
