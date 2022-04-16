be94d67972833a94a28e57ffe008024eff877b41
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2022-04-16 12:36:21 +0300

    Add files via upload

---
+++ Camp Progress/746. Min Cost Climbing Stairs.cpp
@@ -0,0 +1,18 @@
+// Question Link: https://leetcode.com/problems/min-cost-climbing-stairs
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        cost.push_back(0);
+        int len = cost.size();
+        
+        vector<int> dp(len, 0);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < len; i++) {
+            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        
+        return dp.back();
+    }
+};
