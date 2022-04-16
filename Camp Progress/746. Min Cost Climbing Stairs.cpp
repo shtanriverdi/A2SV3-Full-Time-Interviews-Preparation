ff277194b092e3bb0fba13614edcf1ea1745d000
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2022-04-16 12:53:13 +0300

    Update 746. Min Cost Climbing Stairs.cpp

--- Camp Progress/746. Min Cost Climbing Stairs.cpp
+++ Camp Progress/746. Min Cost Climbing Stairs.cpp
@@ -16,3 +16,21 @@ public:
         return dp.back();
     }
 };
+
+// Python3 Version
+/*
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        
+        n = len(cost)
+        
+        dp = [0] * n
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        
+        for i in range(n):
+            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
+
+        return dp[-1]
+*/
