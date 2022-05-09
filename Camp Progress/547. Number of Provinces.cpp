399b379120253146249ebaa15524e42c36637156
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2022-05-09 17:21:45 +0300

    Add files via upload

--- Camp Progress/547. Number of Provinces.cpp
+++ Camp Progress/547. Number of Provinces.cpp
@@ -2,26 +2,53 @@
 
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>> &adjMatrix) {
-        int N = adjMatrix.size(), count = 0;
-        for (int i = 0; i < N; i++) {
-            if (adjMatrix[i][i] == 1) {
-                dfs(adjMatrix, i, N);
-                count++;
-            }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> rank(n, 1);
+        vector<int> dsu(n);
+        for (int i = 0; i < n; i++) {
+            dsu[i] = i;
         }
         
-        return count;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    Union(i, j, dsu, rank);
+                    isConnected[i][j] = 0;
+                    isConnected[j][i] = 0;
+                }
+            }   
+        }
+        
+        unordered_set<int> provinces;
+        for (int i = 0; i < n; i++) {
+            provinces.insert(find(dsu[i], dsu));
+        }
+        
+        return provinces.size();
     }
     
-    void dfs(vector<vector<int>> &adjMatrix, int cur, int &N) {
-        adjMatrix[cur][cur] = 0;
-        for (int i = 0; i < N; i++) {
-            if (adjMatrix[cur][i] == 1) {
-                adjMatrix[cur][i] = 0;
-                adjMatrix[i][cur] = 0;
-                dfs(adjMatrix, i, N);
-            }
+    int find(int cur, vector<int> &dsu) {
+        if (dsu[cur] == cur) {
+            return cur;
         }
+        dsu[cur] = find(dsu[cur], dsu);
+        return dsu[cur];
+    }
+    
+    void Union(int a, int b, vector<int> &dsu, vector<int> &rank) {
+        int A = find(a, dsu);
+        int B = find(b, dsu);
+        
+        if (A == B) {
+            return;
+        }
+        
+        if (rank[B] > rank[A]) {
+            swap(B, A);
+        }
+        dsu[B] = A;
+        
+        rank[A] += rank[B];
     }
 };
