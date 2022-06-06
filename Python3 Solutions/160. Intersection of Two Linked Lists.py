d855539d50eb8759fb349fcec2682dbe8a2a7306
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2022-06-06 15:14:15 +0300

    Update 160. Intersection of Two Linked Lists.py

--- Python3 Solutions/160. Intersection of Two Linked Lists.py
+++ Python3 Solutions/160. Intersection of Two Linked Lists.py
@@ -6,6 +6,49 @@
 #         self.val = x
 #         self.next = None
 
+# Constant Space Solution
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        walk_a = headA
+        len_a = 0
+        last_node_a = None
+        while walk_a:
+            len_a += 1
+            if walk_a.next == None:
+                last_node_a = walk_a
+            walk_a = walk_a.next
+            
+        walk_b = headB
+        last_node_b = None
+        len_b = 0
+        while walk_b:
+            len_b += 1
+            if walk_b.next == None:
+                last_node_b = walk_b
+            walk_b = walk_b.next
+            
+        if last_node_b != last_node_a:
+            return None
+        
+        longer_list = headA
+        shorter_list = headB
+        if len_b > len_a:
+            longer_list = headB
+            shorter_list = headA
+            len_a, len_b = len_b, len_a
+        
+        while len_a > len_b:
+            len_a -= 1
+            longer_list = longer_list.next
+        
+        while longer_list and shorter_list and longer_list != shorter_list:
+            longer_list = longer_list.next
+            shorter_list = shorter_list.next
+            
+        return shorter_list
+
+    
+# Linear Space Solution
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         seen = set()
