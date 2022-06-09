ae6d16d2edfaef960e0e8efdb4d0a88e15d1c2ec
Süha Tanrıverdi <36234545+shtanriverdi@users.noreply.github.com>
2022-06-09 14:52:12 +0300

    Update 167. Two Sum II - Input Array Is Sorted.cpp

--- Camp Progress/167. Two Sum II - Input Array Is Sorted.cpp
+++ Camp Progress/167. Two Sum II - Input Array Is Sorted.cpp
@@ -1,5 +1,28 @@
 // Question Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 
+// Time: O(n) Two Pointers
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int sum, left = 0, right = numbers.size() - 1;
+        while (left < right) {
+            sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return { left + 1, right + 1 };
+            }
+            else if (sum > target) {
+                right--;
+            }
+            else {
+                left++;
+            }
+        }
+        
+        return { 1453, 1299 };
+    }
+};
+
+// Time: O(nlogn) Binary Search
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
